93e33539d5782ec37ff34629cc35d0324711ac9d
Benjamin KABEYA **Afrik Interim HR Services ** <benjamin.kabeya@afrikinterim.local>
2024-10-29 18:02:20 +0100

    init project with index.html styled

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+/*===== Fonction d'activation du boutton menu au format mobile =====*/ 
+const showMenu = (toggleId, navId)=>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+  
+    if(toggle && nav){
+      toggle.addEventListener('click', ()=>{
+        nav.classList.toggle('show')
+        toggle.classList.toggle('bx-x')
+      })
+    }
+  }
+  showMenu('header-toggle','nav-menu')
+
+  /*===== ouvrir et fermer le mu =====*/
+const navLink = document.querySelectorAll('.nav__link');   
+
+function linkAction(){
+  /*Active link*/
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
---
+++ scrollReveal.js
@@ -0,0 +1,29 @@
+const scrollBlock = ScrollReveal()
+scrollBlock.reveal('.about', { 
+    origin:'top',
+    distance:'30px',
+    duration: 700,
+    delay: 500,
+    scale:0.5,
+    reset:true
+});
+
+// scrollBlock.reveal('section', { 
+//     origin:'top',
+//     distance:'30px',
+//     duration: 700,
+//     delay: 500,
+//     scale:0.5,
+//     reset:true
+// },500);
+
+
+
+scrollBlock.reveal('button',{},700)
+
+scrollBlock.reveal('.imgContent',{},700)
+
+scrollBlock.reveal('p',{},700)
+
+scrollBlock.reveal('.item',{},700)
+scrollBlock.reveal('h1',{},700)
